ede7256dc571c851d0255741b8e4876aefc237ed
Romain Ruaud <romain.ruaud@gmail.com>
2026-06-10 22:43:33 +0200

    Adding Recommenders in Product page

--- src/Command/StructureSync.php
+++ src/Command/StructureSync.php
@@ -32,6 +32,7 @@ class StructureSync extends Command
         'catalog' => 'syncAllLocalizedCatalogs',
         'sourceField' => 'syncAllSourceFields',
         'sourceFieldOption' => 'syncAllSourceFieldOptions',
+        'recommenderType' => 'syncAllRecommenderTypes',
     ];
 
     public function __construct(
